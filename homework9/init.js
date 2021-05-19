3d7f6018cc453ad182e3fd4fc431124a6225eb3d
UrielAnimeLover <uriell2mad@gmail.com>
2021-05-19 13:46:58 +0300

    fixed homework 9

--- homework9/init.js
+++ homework9/init.js
@@ -1,10 +1,10 @@
 let btnEl = document.querySelector('.todo-btn');
-let inputEl = document.querySelector(".todo-input");
-let ulEl = document.querySelector('.todo-list')
+let inputEl = document.querySelector('.todo-input');
+let ulEl = document.querySelector('.todo-list');
 
 btnEl.onclick = function() {
   let liEl = document.createElement('li');
   liEl.innerText = inputEl.value;
   ulEl.append(liEl);
-  inputEl.value = "";
+  inputEl.value = '';
 }
