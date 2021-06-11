c468758af3ffab02bfb0cd495e0a234f6a41ac30
UrielAnimeLover <uriell2mad@gmail.com>
2021-06-11 13:34:23 +0300

    prettier fix

--- homework13/init.js
+++ homework13/init.js
@@ -6,54 +6,56 @@ function Accordion(element) {
   this.openAll = openAll;
   this.closeAll = closeAll;
 }
-const mainAccordion = new Accordion(document.getElementById('accordion'));
-let openAllBtnEl = document.querySelector('.open-all-btn');
-let closeAllBtnEl = document.querySelector('.close-all-btn');
-openAllBtnEl.addEventListener('click', openAll.bind(mainAccordion));
-closeAllBtnEl.addEventListener('click', closeAll.bind(mainAccordion));
+const mainAccordion = new Accordion(document.getElementById("accordion"));
+let openAllBtnEl = document.querySelector(".open-all-btn");
+let closeAllBtnEl = document.querySelector(".close-all-btn");
+openAllBtnEl.addEventListener("click", openAll.bind(mainAccordion));
+closeAllBtnEl.addEventListener("click", closeAll.bind(mainAccordion));
 addEventForTitles();
-const addBlockBtn = document.querySelector('.add-block-btn');
-addBlockBtn.addEventListener('click', addBlockFunction);
-//functions 
+const addBlockBtn = document.querySelector(".add-block-btn");
+addBlockBtn.addEventListener("click", addBlockFunction);
+//functions
 function addBlockFunction() {
-  const titleElValue = document.querySelector('.title-value').value;
-  const descElValue = document.querySelector('.description-value').value;
+  const titleElValue = document.querySelector(".title-value").value;
+  const descElValue = document.querySelector(".description-value").value;
   mainAccordion.addBlock(titleElValue, descElValue);
 }
 function addEventForTitles() {
-  document.querySelectorAll('.title').forEach((e) => {
-    e.addEventListener('click', onClickToggle);
+  document.querySelectorAll(".title").forEach((e) => {
+    e.addEventListener("click", onClickToggle);
   });
 }
 function onClickToggle() {
-  this.parentNode.querySelector('.content').classList.toggle('active');
+  this.parentNode.querySelector(".content").classList.toggle("active");
 }
 function toggle(index) {
-  this.element.querySelectorAll('.content')[index].classList.toggle('active');
+  this.element.querySelectorAll(".content")[index].classList.toggle("active");
 }
 function addBlock(title, description) {
-  let blockEl = document.createElement('div');
-  let blockTemplate = document.querySelector('#newBlock').innerHTML;
-  blockEl.innerHTML = blockTemplate.replace('{{title}}', title).replace('{{content}}', description);
+  let blockEl = document.createElement("div");
+  let blockTemplate = document.querySelector("#newBlock").innerHTML;
+  blockEl.innerHTML = blockTemplate
+    .replace("{{title}}", title)
+    .replace("{{content}}", description);
   this.element.append(blockEl);
   addEventForTitles();
 }
 function openAll() {
-  this.element.querySelectorAll('.content').forEach((e) => {
-    e.classList.add('active');
+  this.element.querySelectorAll(".content").forEach((e) => {
+    e.classList.add("active");
   });
 }
 function closeAll() {
-  this.element.querySelectorAll('.content').forEach((e) => {
-    e.classList.remove('active');
+  this.element.querySelectorAll(".content").forEach((e) => {
+    e.classList.remove("active");
   });
 }
 function getOpenIndexes() {
-  let activeElement = this.element.querySelectorAll('.content');
+  let activeElement = this.element.querySelectorAll(".content");
   let activeElArray = [];
-  for(let i = 0; i < activeElement.length; i++) {
-    if(activeElement[i].classList.contains('active')) {
-     activeElArray.push(i);
+  for (let i = 0; i < activeElement.length; i++) {
+    if (activeElement[i].classList.contains("active")) {
+      activeElArray.push(i);
     }
   }
   return activeElArray;
