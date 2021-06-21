34c61ba06a96ac3949376de1b33507cb428b7fbb
UrielAnimeLover <uriell2mad@gmail.com>
2021-06-21 13:48:12 +0300

    prettier fix

--- homework14/init.js
+++ homework14/init.js
@@ -1,15 +1,15 @@
 class Burger {
   static SIZES = {
-    small: 'small',
-    average: 'average',
-    big: 'big'
+    small: "small",
+    average: "average",
+    big: "big",
   };
   static TOPPINGS = {
-    cheese: {price: 10, kcal: 20},
-    salad: {price: 20, kcal: 5},
-    potato: {price: 15, kcal: 10},
-    spice: {price: 15, kcal: 0},
-    mayo: {price: 20, kcal: 5}
+    cheese: { price: 10, kcal: 20 },
+    salad: { price: 20, kcal: 5 },
+    potato: { price: 15, kcal: 10 },
+    spice: { price: 15, kcal: 0 },
+    mayo: { price: 20, kcal: 5 },
   };
   toppings = [];
   constructor(size) {
@@ -17,54 +17,60 @@ class Burger {
     this.toppings = [];
     this.properties = {
       [Burger.SIZES.small]: {
-        price: 50, kcal: 20
+        price: 50,
+        kcal: 20,
       },
       [Burger.SIZES.average]: {
-        price: 75, kcal: 30
+        price: 75,
+        kcal: 30,
       },
       [Burger.SIZES.big]: {
-        price: 100, kcal: 40
-      }
+        price: 100,
+        kcal: 40,
+      },
     };
     this._init();
-  };
+  }
   _init() {
     let burgerProperties = this.properties[this.size];
     this.price = burgerProperties ? burgerProperties.price : 0;
     this.kcal = burgerProperties ? burgerProperties.kcal : 0;
-  };
+  }
   getPrice() {
     let toppingsPrice = 0;
-    this.toppings.forEach(e => {
+    this.toppings.forEach((e) => {
       toppingsPrice = toppingsPrice + e.price;
     });
     return toppingsPrice + this.price;
-  };
+  }
   getKcal() {
     let kcalSum = 0;
-    this.toppings.forEach(e => {
+    this.toppings.forEach((e) => {
       kcalSum = kcalSum + e.kcal;
-    }); 
+    });
     return kcalSum + this.kcal;
-  };
+  }
   addTopping(topping) {
     this.toppings.push(topping);
   }
-};
+}
 class BigMaс extends Burger {
   constructor(size) {
     super();
     this.size = size;
     this.properties = {
       [Burger.SIZES.small]: {
-        price: 50, kcal: 20
+        price: 50,
+        kcal: 20,
       },
       [Burger.SIZES.average]: {
-        price: 75, kcal: 30
+        price: 75,
+        kcal: 30,
       },
       [Burger.SIZES.big]: {
-        price: 100, kcal: 40
-      }
+        price: 100,
+        kcal: 40,
+      },
     };
     this._init();
   }
